b743810f164c2e587ee9da74e22e0e3d46bbbd4a
chaudhuree <chaudhuree00@gmail.com>
2023-07-24 08:03:17 +0600

    assignment completed

--- src/App.vue
+++ src/App.vue
@@ -1,67 +1,33 @@
 <script setup>
-import { ref } from "vue"
-const message = ref("Hello Vue 3")
+import { ref } from 'vue'
+const refIndex = ref(1)
 
-const space = ref(0)
-
-function spacePressed(){
-  space.value++
-  message.value = "Space pressed " + space.value + " times"
+const toggleButton = (n) => {
+   
+    if (refIndex.value === n) {
+        refIndex.value = 0
+    } else {
+        refIndex.value = n
+    }
 }
 
-function pressedWhat(event){
-  message.value = "You pressed " + event.key 
-}
-
-function buttonPressed(){
-  message.value = "You clicked the button"
-}
-
-function ctrlRightClicked(){
-  message.value = "Ctrl+Right Click"
-}
-
-function divClicked(){
-  alert("Div Clicked")
-}
-function buttonClicked(){
-  alert("Button Clicked")
-}
 </script>
 
 <template>
   <section class="container mx-auto flex items-center flex-col">
-    <h1 class="text-center text-2xl py-10">Events & Reactivity in Vue.js</h1>
-    <h2 class="mt-10">{{ message }}</h2>
-    <form @submit.prevent action="https://google.com/search">
-      <div class="container mx-auto flex space-x-5 justify-center m-5">
-        <input @keyup.space="spacePressed()" name="q" type="text"  class="border border-gray-500 bg-white p-5"  placeholder="Press Space">
-      </div>
-      
-      <div class="container mx-auto flex space-x-5 justify-center m-5">
-        <input @keyup="pressedWhat($event)" name="text" type="text" class="border border-gray-500 bg-white p-5"  placeholder="Type Something">
-      </div>
-
-      <button @click.prevent="buttonPressed()" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" >
-        Button
-      </button>
-
-      <button @click.ctrl="ctrlRightClicked()" class="bg-blue-500 ml-5 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" >
-        Ctrl + Right Click
-      </button>
-
-    </form>
-
-    <div @click="divClicked()" class="bg-gray-200 w-[500px] h-80 mt-5 flex items-center justify-center"  >
-      <!--  stop propagation -->
-      <!-- if we do not use stop then when we will click on the button
-      then two alert will be shown one from button and one from div
-      -->
-      <button @click.stop="buttonClicked()" class="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded"  >
-        Button
-      </button>
-    </div>
-  </section>
+        <h1 class="text-center text-2xl py-10">Assignment</h1>
+        <p class="p-5">Make These Buttons Work</p>
+        <div class="flex justify-between space-x-6">
+            <div class="p-10 border w-96 flex flex-col space-y-5">
+                <button @click="toggleButton(n)" v-for="(n,index) in 3" :key="n" class="text-white font-bold py-2 px-4 rounded" :class="refIndex==n ?'bg-blue-500 hover:bg-blue-700':'bg-gray-500 hover:bg-gray-700' ">
+                    Light {{ n }} On
+                </button>
+            </div>
+            <div class="p-10 border w-96 flex flex-col space-y-5">
+                <img v-for="n in 3" class="w-20" :src="refIndex==n?'//img.icons8.com/?size=512&id=12244&format=png':'//img.icons8.com/?size=512&id=75&format=png'" :alt="refIndex==n?'light_on':'light_off'">
+            </div>
+        </div>
+    </section>
 </template>
 
 <style scoped></style>
