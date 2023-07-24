d02511b4b7940e5aca9565021c3088bfc689baba
chaudhuree <chaudhuree00@gmail.com>
2023-07-24 08:03:17 +0600

    assignment completed

---
+++ src/assets/data.js
@@ -0,0 +1,13 @@
+const image = [
+  <img
+    class="w-20"
+    src="//img.icons8.com/?size=512&id=12244&format=png"
+    alt=""
+  />,
+  <img
+    class="w-20"
+    src="//img.icons8.com/?size=512&id=12244&format=png"
+    alt=""
+  />,
+  <img class="w-20" src="//img.icons8.com/?size=512&id=75&format=png" alt="" />,
+];
